f64a3187a50b32d54be05dd5e7de180315945d16
Danyel Chuquipoma <percypdf99@gmail.com>
2026-04-28 17:07:54 -0500

    Se creo la interfaz pago

---
+++ src/main/java/com/example/ProyectoAgronomiaGrupo5/Repository/IPagosRepository.java
@@ -0,0 +1,10 @@
+package com.example.ProyectoAgronomiaGrupo5.Repository;
+
+import com.example.ProyectoAgronomiaGrupo5.Models.Pago;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IPagosRepository extends JpaRepository <Pago, Integer> {
+}
+
+
+
