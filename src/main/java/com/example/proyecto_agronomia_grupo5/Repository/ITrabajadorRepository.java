539e559b063d5fbed522f87a646802a6ad61cddb
ShiroGH26 <chupetesempai@gmail.com>
2026-04-27 22:46:06 -0500

    crear repository de Trabajador

---
+++ src/main/java/com/example/proyecto_agronomia_grupo5/Repository/ITrabajadorRepository.java
@@ -0,0 +1,7 @@
+package com.example.proyecto_agronomia_grupo5.Repository;
+
+import com.example.proyecto_agronomia_grupo5.Models.Trabajador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITrabajadorRepository extends JpaRepository<Trabajador, Integer> {
+}
