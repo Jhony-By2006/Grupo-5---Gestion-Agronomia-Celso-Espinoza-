aaf4484187e3647e0dbad93260be7bf0a3e60937
ShiroGH26 <chupetesempai@gmail.com>
2026-04-27 21:51:43 -0500

    feat: crear repositorio de ProductoFinal

---
+++ src/main/java/com/example/proyecto_agronomia_grupo5/Repository/IProductoFinalRepository.java
@@ -0,0 +1,7 @@
+package com.example.proyecto_agronomia_grupo5.Repository;
+
+import com.example.proyecto_agronomia_grupo5.Models.ProductoFinal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProductoFinalRepository extends JpaRepository<ProductoFinal, Integer> {
+}
