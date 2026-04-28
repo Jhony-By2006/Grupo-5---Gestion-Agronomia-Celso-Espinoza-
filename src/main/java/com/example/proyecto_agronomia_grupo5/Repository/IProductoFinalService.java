92ad29557ee5139b6846af235a6ac4050c361b63
ShiroGH26 <chupetesempai@gmail.com>
2026-04-27 22:24:34 -0500

    crear service de ProductoFinal

---
+++ src/main/java/com/example/proyecto_agronomia_grupo5/Repository/IProductoFinalService.java
@@ -0,0 +1,17 @@
+package com.example.proyecto_agronomia_grupo5.Service;
+
+import com.example.proyecto_agronomia_grupo5.Models.ProductoFinal;
+import java.util.List;
+
+public interface IProductoFinalService {
+
+    ProductoFinal save(ProductoFinal productoFinal) throws Exception;
+
+    ProductoFinal update(ProductoFinal productoFinal, Integer id) throws Exception;
+
+    List<ProductoFinal> findAll() throws Exception;
+
+    ProductoFinal findById(Integer id) throws Exception;
+
+    void delete(Integer id) throws Exception;
+}
