fb1ccdc2384cb7d72bdbaa2dcaa18ac3e596ddd8
ShiroGH26 <chupetesempai@gmail.com>
2026-05-21 01:54:56 -0500

    Add Trabajador interface with relevant fields

--- src/app/models/src/app/models/trabajador.ts
+++ src/app/models/src/app/models/trabajador.ts
@@ -1,1 +1,18 @@
+export interface Trabajador {
+  id?: number;
+
+  nombre: string;
+
+  apellido: string;
+
+  dni: string;
+
+  cargo: string;
+
+  telefono?: string;
+
+  direccion?: string;
+
+  estado?: string;
+}
 
