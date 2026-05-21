4371c2d9c2e0e034e088d4cac08590674b085b83
ShiroGH26 <chupetesempai@gmail.com>
2026-05-21 01:44:17 -0500

    revirtiendo cambios

--- src/app/models/src/app/models/trabajador.ts
+++ src/app/models/src/app/models/trabajador.ts
@@ -1,17 +1,1 @@
-export interface Trabajador {
-  id?: number;
 
-  nombre: string;
-
-  apellido: string;
-
-  dni: string;
-
-  cargo: string;
-
-  telefono?: string;
-
-  direccion?: string;
-
-  estado?: string;
-}
