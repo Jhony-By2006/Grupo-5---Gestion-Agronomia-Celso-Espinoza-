e7fb9112fb53bcd59d99b347574f3714e367bfbc
ShiroGH26 <chupetesempai@gmail.com>
2026-05-20 23:55:05 -0500

    Add Trabajador interface for worker model

---
+++ src/app/models/src/app/models/trabajador.ts
@@ -0,0 +1,17 @@
+export interface Trabajador {
+  id?: number;
+
+  nombre: string;
+
+  apellido: string;
+
+  dni: string;
+
+  cargo: string;
+
+  telefono?: string;
+
+  direccion?: string;
+
+  estado?: string;
+}
